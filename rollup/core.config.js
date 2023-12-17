db99ba21dba6b429c1c2697999e8aefbf4c61ad6
Jeff Glass <glass.jeffrey@gmail.com>
2023-12-17 17:52:00 -0600

    Add rollup-plugin-string

--- rollup/core.config.js
+++ rollup/core.config.js
@@ -3,14 +3,17 @@
 
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
+import { string } from "rollup-plugin-string"
 
 import { createRequire } from "node:module";
 
 createRequire(import.meta.url)("./build_xworker.cjs");
 
+const plugins = [nodeResolve(), string({ include: "**/*.py" })];
+
 export default {
     input: "./esm/index.js",
-    plugins: process.env.NO_MIN ? [nodeResolve()] : [nodeResolve(), terser()],
+    plugins: process.env.NO_MIN ? plugins : plugins.concat(terser()),
     output: {
         esModule: true,
         file: "./core.js",
--- rollup/xworker.config.js
+++ rollup/xworker.config.js
@@ -3,6 +3,7 @@
 
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
+import { string } from "rollup-plugin-string"
 
 import { createRequire } from "node:module";
 import { fileURLToPath } from "node:url";
@@ -14,9 +15,11 @@ const WORKERS_DIR = resolve(
     join(dirname(fileURLToPath(import.meta.url)), "..", "esm", "worker"),
 );
 
+const plugins = [nodeResolve(), string({ include: "**/*.py" })];
+
 export default {
     input: join(WORKERS_DIR, "_template.js"),
-    plugins: process.env.NO_MIN ? [nodeResolve()] : [nodeResolve(), terser()],
+    plugins: process.env.NO_MIN ? plugins : plugins.concat(terser()),
     output: {
         esModule: true,
         file: join(WORKERS_DIR, "__template.js"),
