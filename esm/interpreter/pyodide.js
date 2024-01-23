68a7efa369967607e24eae16e793ffe66b749457
webreflection <andrea.giammarchi@gmail.com>
2024-01-23 17:15:44 +0100

    [experiment] Automatically patch Pyodide callbacks on main

--- esm/interpreter/pyodide.js
+++ esm/interpreter/pyodide.js
@@ -1,9 +1,80 @@
+import { create } from 'gc-hook';
+
 import { fetchFiles, fetchJSModules, fetchPaths, writeFile } from './_utils.js';
 import { registerJSModule, run, runAsync, runEvent } from './_python.js';
 import { stdio } from './_io.js';
 
+const RUNNING_ON_MAIN = !!globalThis.window;
+
 const type = 'pyodide';
 const toJsOptions = { dict_converter: Object.fromEntries };
+
+/* c8 ignore start */
+let overrideFunction = false;
+const overrideMethod = method => (...args) => {
+    try {
+        overrideFunction = true;
+        return method(...args);
+    }
+    finally {
+        overrideFunction = false;
+    }
+};
+
+let overridden = false;
+const applyOverride = () => {
+    if (overridden) return;
+    overridden = true;
+
+    const proxies = new WeakMap;
+    const onGC = value => value.destroy();
+    const patchArgs = args => {
+        for (let i = 0; i < args.length; i++) {
+            const value = args[i];
+            if (
+                typeof value === 'function' &&
+                'copy' in value
+            ) {
+                // avoid seppuku / Harakiri + speed up
+                overrideFunction = false;
+                // reuse copied value if known already
+                let proxy = proxies.get(value)?.deref();
+                if (!proxy) {
+                    try {
+                        // observe the copy and return a Proxy reference
+                        proxy = create(value.copy(), onGC);
+                        proxies.set(value, new WeakRef(proxy));
+                    }
+                    catch (error) {
+                        console.error(error);
+                    }
+                }
+                if (proxy) args[i] = proxy;
+                overrideFunction = true;
+            }
+        }
+    };
+
+    // trap apply to make call possible after the patch
+    const { call } = Function;
+    const apply = call.bind(call, call.apply);
+    // the patch
+    Object.defineProperties(Function.prototype, {
+        apply: {
+            value(context, args) {
+                if (overrideFunction) patchArgs(args);
+                return apply(this, context, args);
+            }
+        },
+        call: {
+            value(context, ...args) {
+                if (overrideFunction) patchArgs(args);
+                return apply(this, context, args);
+            }
+        }
+    });
+};
+/* c8 ignore stop */
 
 // REQUIRES INTEGRATION TEST
 /* c8 ignore start */
@@ -12,6 +83,9 @@ export default {
     module: (version = '0.25.0') =>
         `https://cdn.jsdelivr.net/pyodide/v${version}/full/pyodide.mjs`,
     async engine({ loadPyodide }, config, url) {
+        // apply override ASAP then load foreign code
+        if (RUNNING_ON_MAIN && config.experimental_create_proxy === 'auto')
+            applyOverride();
         const { stderr, stdout, get } = stdio();
         const indexURL = url.slice(0, url.lastIndexOf('/'));
         const interpreter = await get(
@@ -29,11 +103,11 @@ export default {
         return interpreter;
     },
     registerJSModule,
-    run,
-    runAsync,
-    runEvent,
-    transform: (interpreter, value) => (
-        value instanceof interpreter.ffi.PyProxy ?
+    run: overrideMethod(run),
+    runAsync: overrideMethod(runAsync),
+    runEvent: overrideMethod(runEvent),
+    transform: ({ ffi: { PyProxy } }, value) => (
+        value instanceof PyProxy ?
             value.toJs(toJsOptions) :
             value
     ),
