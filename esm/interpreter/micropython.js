bd3ef615767da3048d0cd587d1eed23b13d75f39
webreflection <andrea.giammarchi@gmail.com>
2024-05-31 10:53:42 +0200

    Updated MicroPython and devDependencies

--- esm/interpreter/micropython.js
+++ esm/interpreter/micropython.js
@@ -23,7 +23,7 @@ const mkdir = (FS, path) => {
 
 export default {
     type,
-    module: (version = '1.22.0-380') =>
+    module: (version = '1.23.0') =>
         `https://cdn.jsdelivr.net/npm/@micropython/micropython-webassembly-pyscript@${version}/micropython.mjs`,
     async engine({ loadMicroPython }, config, url, baseURL) {
         const { stderr, stdout, get } = stdio({
