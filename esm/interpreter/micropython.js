1f811a9b9cb60c0604a5c58db680b690c95c3a5a
webreflection <andrea.giammarchi@gmail.com>
2024-06-18 12:47:22 +0200

    0.13.0

--- esm/interpreter/micropython.js
+++ esm/interpreter/micropython.js
@@ -23,7 +23,7 @@ const mkdir = (FS, path) => {
 
 export default {
     type,
-    module: (version = '1.23.0') =>
+    module: (version = '1.24.0-preview-44') =>
         `https://cdn.jsdelivr.net/npm/@micropython/micropython-webassembly-pyscript@${version}/micropython.mjs`,
     async engine({ loadMicroPython }, config, url, baseURL) {
         const { stderr, stdout, get } = stdio({
