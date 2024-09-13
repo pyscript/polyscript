92c15b12b4ffd8feecf54366332032dee0e1972c
webreflection <andrea.giammarchi@gmail.com>
2024-09-13 16:50:32 +0200

    Follow up on https://github.com/pyscript/pyscript/pull/2168

--- rollup/build_python.cjs
+++ rollup/build_python.cjs
@@ -4,6 +4,8 @@ const { join, resolve } = require('node:path');
 const { readdirSync, writeFileSync } = require('node:fs');
 const { spawnSync } = require("node:child_process");
 
+const dedent = require('codedent');
+
 const PYTHON_DIR = resolve(join(__dirname, '..', 'python'));
 const PYTHON_JS_DIR = resolve(join(__dirname, '..', 'esm', 'python'));
 
@@ -11,14 +13,35 @@ const { stringify } = JSON;
 
 for (const file of readdirSync(PYTHON_DIR)) {
     const full = join(PYTHON_DIR, file);
-    const {
-        output: [error, result],
-    } = spawnSync("pyminify", [
-        "--remove-literal-statements",
-        full,
-    ]);
-    if (error) process.exit(1);
-    const python = stringify(result.toString());
+    let python = '';
+    try {
+        const {
+            output: [error, result],
+        } = spawnSync("pyminify", [
+            "--remove-literal-statements",
+            full,
+        ]);
+        if (error) {
+            console.error(error);
+            process.exit(1);
+        }
+        python = stringify(result.toString());
+    }
+    catch (error) {
+        console.error(error);
+        console.log(dedent(`
+            \x1b[1m⚠️  is your env activated?\x1b[0m
+            \x1b[2mYou need a Python env to run \x1b[0mpyminify\x1b[2m.\x1b[0m
+            \x1b[2mTo do so, you can try the following:\x1b[0m
+            python -m venv env
+            source env/bin/activate
+            pip install --upgrade pip
+            pip install --ignore-requires-python python-minifier
+            pip install setuptools
+            \x1b[2mand you can then try \x1b[0mnpm run build\x1b[2m again.\x1b[0m
+        `));
+        process.exit(1);
+    }
     writeFileSync(
         join(PYTHON_JS_DIR, file.replace(/\.py$/, '.js')),
         `// ⚠️ DO NOT MODIFY - SOURCE FILE: "../../python/${file}"
