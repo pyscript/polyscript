13bba3537910815fe5194d21141d943140e2e142
webreflection <andrea.giammarchi@gmail.com>
2024-10-16 15:37:37 +0200

    Export the buffered utility too

--- esm/exports.js
+++ esm/exports.js
@@ -1,5 +1,7 @@
 // this file simply exports enough stuff to allow
 // 3rd party libraries, including PyScript, to work
+import { buffered } from './interpreter/_io.js';
+export { buffered };
 export * from './index.js';
 export * from './script-handler.js';
 export * from './utils.js';
