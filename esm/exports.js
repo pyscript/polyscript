822a1914f6bb6e18f8a6ba934dbed976eca10ac0
webreflection <andrea.giammarchi@gmail.com>
2025-03-10 11:50:33 +0100

    Added loadProgress export

--- esm/exports.js
+++ esm/exports.js
@@ -1,7 +1,8 @@
 // this file simply exports enough stuff to allow
 // 3rd party libraries, including PyScript, to work
 import { buffered } from './interpreter/_io.js';
-export { buffered };
+import { loadProgress } from './interpreter/_utils.js';
+export { buffered, loadProgress };
 export * from './index.js';
 export * from './script-handler.js';
 export * from './utils.js';
