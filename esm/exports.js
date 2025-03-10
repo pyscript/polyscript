2e1d75427a98cc7fdd31879c5c36be9ef798c6e7
webreflection <andrea.giammarchi@gmail.com>
2025-03-10 12:08:20 +0100

    Simplified loadProgress by type

--- esm/exports.js
+++ esm/exports.js
@@ -1,7 +1,11 @@
 // this file simply exports enough stuff to allow
 // 3rd party libraries, including PyScript, to work
 import { buffered } from './interpreter/_io.js';
-import { loadProgress } from './interpreter/_python.js';
+import { loadProgress as lP } from './interpreter/_python.js';
+import { registry } from './interpreters.js';
+
+const loadProgress = (type, ...rest) => lP(registry.get(type), ...rest);
+
 export { buffered, loadProgress };
 export * from './index.js';
 export * from './script-handler.js';
