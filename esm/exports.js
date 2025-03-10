98b13d5d24f968fea125e44180c208a1dfe2a374
webreflection <andrea.giammarchi@gmail.com>
2025-03-10 12:28:15 +0100

    Added createProgress too

--- esm/exports.js
+++ esm/exports.js
@@ -1,12 +1,13 @@
 // this file simply exports enough stuff to allow
 // 3rd party libraries, including PyScript, to work
 import { buffered } from './interpreter/_io.js';
+import { createProgress } from './interpreter/_utils.js';
 import { loadProgress as lP } from './interpreter/_python.js';
 import { registry } from './interpreters.js';
 
 const loadProgress = (type, ...rest) => lP(registry.get(type), ...rest);
 
-export { buffered, loadProgress };
+export { buffered, createProgress, loadProgress };
 export * from './index.js';
 export * from './script-handler.js';
 export * from './utils.js';
