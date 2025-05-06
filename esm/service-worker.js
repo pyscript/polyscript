2e93d2a6cf37a6082f0f1fe6023c4d20709c2d0a
webreflection <andrea.giammarchi@gmail.com>
2025-05-06 13:23:18 +0200

    better ServiceWorker logic

--- esm/service-worker.js
+++ esm/service-worker.js
@@ -2,23 +2,31 @@ import sabayon from 'https://cdn.jsdelivr.net/npm/sabayon/dist/polyfill.js';
 
 // ignore browsers that already support SharedArrayBuffer
 if (!globalThis.crossOriginIsolated) {
+  const { isArray } = Array;
+  const isOptions = args => args.length && typeof args[0] === 'object' && args[0] !== null;
+
   // early patch Blob to inject sabayon polyfill for service-worker
   globalThis.Blob = class extends Blob {
-    constructor(blobParts, options) {
-      if (options.type === 'text/javascript' && typeof blobParts.at(0) === 'string') {
+    constructor(blobParts, ...args) {
+      if (
+        isOptions(args) &&
+        args[0].type === 'text/javascript' &&
+        isArray(blobParts) &&
+        typeof blobParts.at(0) === 'string'
+      ) {
         blobParts[0] = blobParts[0].replace(
           /^\/\*@\*\//,
           'import "https://cdn.jsdelivr.net/npm/sabayon/dist/polyfill.js";'
         );
       }
-      super(blobParts, options);
+      super(blobParts, ...args);
     }
   };
 
   // early patch Worker to bootstrap sabayon for service-worker
   globalThis.Worker = class extends Worker {
     constructor(url, ...args) {
-      if (args.length) {
+      if (isOptions(args)) {
         const sw = args[0].serviceWorker || args[0].service_worker;
         if (sw) sabayon(sw);
       }
