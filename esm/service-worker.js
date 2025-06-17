9ea9288ea5b7da1f31456dad3d7043121d5ccf24
webreflection <andrea.giammarchi@gmail.com>
2025-06-17 14:58:07 +0200

    0.17.24

--- esm/service-worker.js
+++ esm/service-worker.js
@@ -1,36 +1,38 @@
-import sabayon from 'https://cdn.jsdelivr.net/npm/sabayon/dist/polyfill.js';
+// Let's avoid 404s *but* this should not be needed anymore*
 
-// ignore browsers that already support SharedArrayBuffer
-if (!globalThis.crossOriginIsolated) {
-  const { isArray } = Array;
-  const isOptions = args => args.length && typeof args[0] === 'object' && args[0] !== null;
+// import sabayon from 'https://cdn.jsdelivr.net/npm/sabayon/dist/polyfill.js';
 
-  // early patch Blob to inject sabayon polyfill for service-worker
-  globalThis.Blob = class extends Blob {
-    constructor(blobParts, ...args) {
-      if (
-        isOptions(args) &&
-        args[0].type === 'text/javascript' &&
-        isArray(blobParts) &&
-        typeof blobParts.at(0) === 'string'
-      ) {
-        blobParts[0] = blobParts[0].replace(
-          /^\/\*@\*\//,
-          'import "https://cdn.jsdelivr.net/npm/sabayon/dist/polyfill.js";'
-        );
-      }
-      super(blobParts, ...args);
-    }
-  };
+// // ignore browsers that already support SharedArrayBuffer
+// if (!globalThis.crossOriginIsolated) {
+//   const { isArray } = Array;
+//   const isOptions = args => args.length && typeof args[0] === 'object' && args[0] !== null;
 
-  // early patch Worker to bootstrap sabayon for service-worker
-  globalThis.Worker = class extends Worker {
-    constructor(url, ...args) {
-      if (isOptions(args)) {
-        const sw = args[0].serviceWorker || args[0].service_worker;
-        if (sw) sabayon(sw);
-      }
-      super(url, ...args);
-    }
-  };
-}
+//   // early patch Blob to inject sabayon polyfill for service-worker
+//   globalThis.Blob = class extends Blob {
+//     constructor(blobParts, ...args) {
+//       if (
+//         isOptions(args) &&
+//         args[0].type === 'text/javascript' &&
+//         isArray(blobParts) &&
+//         typeof blobParts.at(0) === 'string'
+//       ) {
+//         blobParts[0] = blobParts[0].replace(
+//           /^\/\*@\*\//,
+//           'import "https://cdn.jsdelivr.net/npm/sabayon/dist/polyfill.js";'
+//         );
+//       }
+//       super(blobParts, ...args);
+//     }
+//   };
+
+//   // early patch Worker to bootstrap sabayon for service-worker
+//   globalThis.Worker = class extends Worker {
+//     constructor(url, ...args) {
+//       if (isOptions(args)) {
+//         const sw = args[0].serviceWorker || args[0].service_worker;
+//         if (sw) sabayon(sw);
+//       }
+//       super(url, ...args);
+//     }
+//   };
+// }
