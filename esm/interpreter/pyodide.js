82962845c28ac73cd438c86dbe8cdef2ea840334
webreflection <andrea.giammarchi@gmail.com>
2026-02-03 14:46:24 +0100

    updated Pyodide to version 0.29.3

--- esm/interpreter/pyodide.js
+++ esm/interpreter/pyodide.js
@@ -70,7 +70,7 @@ const indexURLs = new WeakMap();
 
 export default {
     type,
-    module: (version = '0.29.2') =>
+    module: (version = '0.29.3') =>
         `https://cdn.jsdelivr.net/pyodide/v${version}/full/pyodide.mjs`,
     async engine({ loadPyodide, version }, config, url, baseURL) {
         progress('Loading Pyodide');
